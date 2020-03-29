6f8acd08ba4af822f1aa6ac67d907bc3f32a0b60
wenbin <1912315910@qq.com>
2020-03-29 22:06:47 +0800

    update

--- AdvancedPythonKnowledge/datatpye.py
+++ AdvancedPythonKnowledge/datatpye.py
@@ -3,6 +3,7 @@
 # @Author  : Always be codding
 # @Software: PyCharm
 import timeit
+from collections import namedtuple
 
 """
 1.元组和列表
@@ -28,14 +29,36 @@ import timeit
 '''
 
 
-def func():
-    for i in range(1, 4):
-        pass
+# def func():
+#     for i in range(1, 4):
+#         pass
+#
+#
+# res1 = timeit.timeit('(1, 2, 3)')
+# print('元组执行10000000次消耗{}秒'.format(res1))
+# res = timeit.timeit('[1, 2, 3]')
+# print('列表执行10000000次消耗{}秒'.format(res))
+# res2 = timeit.timeit(func)
+# print('方法执行10000000次消耗{}秒'.format(res2))
+
+'''
+1.2 命名元组 规则
+'''
+name_tuple_pattern = namedtuple('name_tuple_info', ['name', 'age', 'gender'])
+
+my_name_tuple = name_tuple_pattern('哈哈哈', 21, gender='男')
+print(my_name_tuple) # name_tuple_info(name='哈哈哈', age=21, gender='男')
+print(my_name_tuple.name) # 哈哈哈
+print(my_name_tuple.age) # 21
+print(my_name_tuple.gender) # 男
+
+''' 
+2.字典和集合的原理和应用
+2.1 散列类型的存储过程
+'''
 
 
-res1 = timeit.timeit('(1, 2, 3)')
-print('元组执行10000000次消耗{}秒'.format(res1))
-res = timeit.timeit('[1, 2, 3]')
-print('列表执行10000000次消耗{}秒'.format(res))
-res2 = timeit.timeit(func)
-print('方法执行10000000次消耗{}秒'.format(res2))
+
+''' 
+2.2 字典查找的过程
+'''
